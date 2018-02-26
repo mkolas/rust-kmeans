22156f32187a437282fee9da2a0eb5764ccd1ada
Max Kolasinski <mkolas@umich.edu>
2018-02-26 16:52:13 -0500

    Still messing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
+#[macro_use]
+extern crate serde_derive;
+
 extern crate csv;
+extern crate serde;
 
 use std::io;
+use std::env;
+use std::process;
+use std::error::Error;
+use std::ffi::OsString;
 
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
 struct Voter {
     // we'll store lat/long as x/y for simplicity's sake
     zip: String,
     x: f64,
-    y: f64
+    y: f64,
 }
 fn run() -> Result<(), Box<Error>> {
-    let file_path = get_first_arg()?;
-    let mut reader = csv::Reader::from_path(file_path);
-    for result in reader.records() {
-        let record = result?;
-        let zip = &record[0];
-        let y: f64 = record[1].parse()?;
-        let x: f64 = record[2].parse()?;
-        println!("{:?}", record);
+    let mut voters = Vec::new();
+
+
+//    let mut reader = csv::Reader::from_path(file_path);
+    let mut reader = csv::Reader::from_reader(io::stdin());
+    for result in reader.deserialize() {
+        let voter: Voter = result?;
+        voters.push(voter);
     }
     Ok(())
 }
 
+#[allow(dead_code)]
 fn get_first_arg() -> Result<OsString, Box<Error>> {
     match env::args_os().nth(1){
         None => Err(From::from("expected 1 argument, got none")),
@@ -31,6 +43,6 @@ fn get_first_arg() -> Result<OsString, Box<Error>> {
 fn main() {
     if let Err(err) = run() {
         println!("{}", err);
-        process::exit();
+        process::exit(1);
     }
 }
